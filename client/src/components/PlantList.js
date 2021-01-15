5b278fe700362ca1678427c8a0ef990fe126ac3a
shawn2776 <=>
2021-01-15 09:57:06 -0800

    finished custom hooks

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,34 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
+  
+ 
+ 
+   // when the component mounts:
+  componentDidMount() {
+     //   - fetch data from the server endpoint - http://localhost:3333/plants
+    axios.get('http://localhost:3333/plants')
+    .then((res) => {
+      console.log(res.data.plantsData)
+        //   - set the returned plants array to this.state.plants
+      this.setState({
+        plants: res.data.plantsData
+      })
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`)
+    })
+  }
+  
+  
+  
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
