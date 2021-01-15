21fb1c0f99ebeaa901516a6c1ac478e452b4b5d5
shawn2776 <=>
2021-01-15 10:26:54 -0800

    completed first test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+test("sanity check test, renders app without errors", () => {
+  render(<CheckoutForm />)
+});
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  // Arrange: render out App component
+  render(<CheckoutForm />)
+  // Get our header DOM
+  const header = screen.getByText("Checkout Form")
+  // Assert: Make sure that the header actually exists
+  console.log(header);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+});
